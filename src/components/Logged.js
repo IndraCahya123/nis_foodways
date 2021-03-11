24e4bc589f8768ead8346b5cd8ed8e279914b4c2
IndraCahya123 <indracahya51@gmail.com>
2021-03-11 19:10:29 +0700

    add dropdown

--- src/components/Logged.js
+++ src/components/Logged.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Cart from '../images/cart.png';
 import Customer from '../images/customer_circle.png';
+import AddProduct from '../images/add_product.png';
+import Logout from '../images/logout.png';
+import UserIcon from '../images/user.png';
 import {Dropdown} from 'react-bootstrap';
 
 function Logged() {
+    const user = 'partner';
     return (
         <>
             <button style={{ background: "none", border: "none" }}>
@@ -24,14 +28,76 @@ function Logged() {
                     />
                 </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </Dropdown.Menu>
+                {user === 'partner' ? <PartnerDropdownMenu /> : <CustomerDropdownMenu />}
             </Dropdown>
         </>
     )
+}
+
+function CustomerDropdownMenu(){
+    return(
+        <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1" className="d-flex align-items-center">
+                <img 
+                    src={UserIcon}
+                    alt="User Icon"
+                    width="25px"
+                    height="25px"
+                    className="mr-2"
+                />
+                <span style={{ fontFamily: "'Nunito Sans'", fontWeight: "800" }}>Profile</span>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item href="#/action-2" className="d-flex align-items-center">
+                <img 
+                    src={Logout}
+                    alt="Logout Icon"
+                    width="25px"
+                    height="25px"
+                    className="mr-2"
+                />
+                <span style={{ fontFamily: "'Nunito Sans'", fontWeight: "800" }}>Logout</span>
+            </Dropdown.Item>
+        </Dropdown.Menu>
+    ); 
+}
+
+function PartnerDropdownMenu(){
+    return(
+        <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1" className="d-flex align-items-center">
+                <img 
+                    src={UserIcon}
+                    alt="User Icon"
+                    width="25px"
+                    height="25px"
+                    className="mr-2"
+                />
+                <span style={{ fontFamily: "'Nunito Sans'", fontWeight: "800" }}>Profile Partner</span>
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-1" className="d-flex align-items-center">
+                <img 
+                    src={AddProduct}
+                    alt="User Icon"
+                    width="25px"
+                    height="25px"
+                    className="mr-2"
+                />
+                <span style={{ fontFamily: "'Nunito Sans'", fontWeight: "800" }}>Add Product</span>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item href="#/action-2" className="d-flex align-items-center">
+                <img 
+                    src={Logout}
+                    alt="Logout Icon"
+                    width="25px"
+                    height="25px"
+                    className="mr-2"
+                />
+                <span style={{ fontFamily: "'Nunito Sans'", fontWeight: "800" }}>Logout</span>
+            </Dropdown.Item>
+        </Dropdown.Menu>
+    ); 
 }
 
 export default Logged
